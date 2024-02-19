cbc1f8143d9f570566185cf71e77617eb8c42e38
amrutha-b-nair <amruthabn00@gmail.com>
2024-02-19 15:24:51 +0100

    PhoneNumber Validator

--- telephoneValidator/script.js
+++ telephoneValidator/script.js
@@ -1,27 +1,26 @@
-// let number = document.getElementById("user-input");
-// let checkButton = document.getElementById("check-btn");
-// let clearButton = document.getElementById("clear-btn");
-// let result = document.getElementById("results-div");
+let number = document.getElementById("user-input");
+let checkButton = document.getElementById("check-btn");
+let clearButton = document.getElementById("clear-btn");
+let result = document.getElementById("results-div");
 
 function validate(num) {
-    const regex = /(1\s?)?(\(\d{3}\)|\d{3})[/s-]?\d{3}[/s-]?\d{4}/;
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
     console.log(num.match(regex));
     return regex.test(num);
 }
 
-// checkButton.addEventListener('click', () => {
-//     if (number.value === ""){
-//         alert("Please provide a phone number");
-//     } else if (validate(number.value)) {
-//         result.textContent = `Valid US number: ${number.value}`;
-//     } else {
-//         result.textContent = `Invalid US number: ${number.value}`;
-//     }
-//     number.value = "";
-// })
+checkButton.addEventListener('click', () => {
+    if (number.value === ""){
+        alert("Please provide a phone number");
+    } else if (validate(number.value)) {
+        result.textContent = `Valid US number: ${number.value}`;
+    } else {
+        result.textContent = `Invalid US number: ${number.value}`;
+    }
+    number.value = "";
+})
 
-// clearButton.addEventListener('click',() => {
-//     result.textContent = "";
-// })
+clearButton.addEventListener('click',() => {
+    result.textContent = "";
+})
 
-console.log(validate("1 (555) 555-5555"));
