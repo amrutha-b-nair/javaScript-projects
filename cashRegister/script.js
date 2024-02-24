d87acbdca5285f474e182e1612fa7e5c849863ca
amrutha-b-nair <amruthabn00@gmail.com>
2024-02-24 19:10:36 +0100

    Cash Registere

--- cashRegister/script.js
+++ cashRegister/script.js
@@ -24,13 +24,38 @@ let changeName = {
 }
 
 
+function giveChange(amountReceived) {
+  let totalDrawer = parseFloat(cid.map(sublist => sublist[1]).reduce((acc, value) => acc + value, 0).toFixed(2));
+  console.log(totalDrawer);
+  console.log(amountReceived - price);
+  if (totalDrawer < amountReceived - price) {
+    console.log("yessss");
+    changeDue.textContent = "Status: INSUFFICIENT_FUNDS";
+  } else if (totalDrawer === amountReceived - price) {
+    console.log("ooooohh")
+  }
+}
+
 const totalAmount = document.getElementById("total-amount");
 const changeDue = document.getElementById("change-due");
 const changeDrawer = document.getElementById("change-drawer");
+const purchaseButton = document.getElementById("purchase-btn");
+const cash = document.getElementById("cash");
 
 totalAmount.textContent =  `Total to Pay: $${price}`;
 
 changeDrawer.innerHTML = `<p><strong>Change in drawer:</strong></p>
 ${cid.map(money => `<p>${changeName[money[0]]}:$${money[1]}</p>`).join('')}`
 
-// let cash = documet.getElementByID()
+purchaseButton.addEventListener('click', () => {
+  if (Number(cash.value) < price){
+      alert("Customer does not have enough money to purchase the item");
+  } else if (Number(cash.value) === price) {
+      console.log("yesss");
+      changeDue.textContent = " No change due - customer paid with exact cash";
+  } else {
+      giveChange(Number(cash.value));
+  }
+
+  cash.value = "";
+})
